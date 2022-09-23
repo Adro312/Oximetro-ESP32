21b24da24944334fe8a2c322e925f3773fd68422
Daniel Ramirez <daniel.rmz.olv@gmail.com>
2022-09-23 13:50:54 -0500

    Se acomodo el codigo para implementar el nuevo sensor

--- OximetroESP32/OximetroESP32.ino
+++ OximetroESP32/OximetroESP32.ino
@@ -1,38 +1,41 @@
+// Librearias para poder conectarse con la API
 #include <ArduinoJson.h>
 #include <EasyHTTP.h>
 #include <WiFi.h>
 
-// Librerias para el sensor DHT11
-#include "DHT.h"
-#define DHTPIN 4
-#define DHTTYPE DHT11
-
-// Detectamos el sensor
-DHT dht(DHTPIN, DHTTYPE);
-
-unsigned long lastTime = 0;
-unsigned long timerDelay = 10000; //Tiempo que tardará en ejecutarse el código en la función loop
-
-//---------- Network info
+// Claves para conectarse al internet
 char* ssid = "Adro312";   //Nombre de la red Ex. "upsrj_ALUMNOS_campus"
 char* password = "4dRo312_E";
 
 // Inicializas el objeto 'http' con los datos de la conexion a internet
 EasyHTTP http(ssid, password);
 
-// Guardas la url base de tu api aqui (con / al final)
-String datosURL = "http://192.168.1.112/Oximetro-API/";
+// Se guarda la URL de la API
+String API_URL = "http://192.168.1.112/Oximetro-API/";
 
 void setup() {
   Serial.begin(115200);
 
-  // Inicializamos el sensor DHT11
-  dht.begin();
+
+  /**
+   * 
+   * 
+   * Colar codigo del nuevo sensor
+   * 
+   * 
+   * 
+  */
+
 
   // Realizas la conexion a la api
-  http.connectWiFi();
-  http.setBaseURL(datosURL);
-  Serial.println("Connected!");
+  if ( http.connectWiFi() ) {
+    Serial.println("Connected!");
+    http.post("API/saveData", "1");
+  } else {
+    Serial.println("Internet don't found");
+  }
+
+  http.setBaseURL(API_URL);
 }
 
 void loop() {
@@ -43,25 +46,32 @@ void loop() {
   // Si el estado es 1, se ejecutara lo siguiente
   if (status_oximetro == "1") {
 
-    // Declaramos las variables que vamos a enviar del sensor de temperatura y humedad
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
+    // Declaramos las variables que vamos a enviar del sensor
+    float hr = 0;
+    float oxi = 0;
+    float temp = 0;
 
-    /* Para hacer un post
-        Se debe de generar un JSON con los datos
+
+    /**
+     * 
+     * 
+     * Colar codigo del nuevo sensor
+     * 
+     * 
+     * 
     */
-    // Inicializas la variable 'doc1' con 128kb de espacio (El default es 32 y te dará error si lo cambias pero te digo por whats como hacerlo xd)
+
+
+    // Se nicializa la variable 'doc1' con 128kb de espacio (El default es 32 y te dará error si lo cambias)
     // De nuevo ten cuidado con el espacio en memoria xd
     DynamicJsonDocument doc1(128);
+
     // Inicializas una cadena llamada payload que tendra los datos que vas a mandar por POST
     String payload1 = "";
 
-    /* Añades los datos que vas a mandar, en mi caso los datos del modelo usuario
-      doc1["nombre_usuario"] = nombre;
-      doc1["correo_usuario"] = correo;
-    */
-    doc1["humedad"] = h;
-    doc1["temperatura"] = t;
+    doc1["oxigenacion"] = oxi;
+    doc1["ritmo_cardiaco"] = hr;
+    doc1["temperatura"] = temp;
     
     serializeJson(doc1, payload1); // Hace la conversión
     
@@ -70,5 +80,5 @@ void loop() {
     Serial.println(payload1);
   }
 
-  delay(5000);
+  delay(3000);
 }
