83f328a7ed84db605bbc219b38d526cdc382d379
Daniel Ramírez <64878115+Adro312@users.noreply.github.com>
2022-10-16 15:41:57 -0500

    V-02

--- OximetroESP32/OximetroESP32.ino
+++ OximetroESP32/OximetroESP32.ino
@@ -1,84 +1,165 @@
-// Librearias para poder conectarse con la API
-#include <ArduinoJson.h>
-#include <EasyHTTP.h>
-#include <WiFi.h>
+#include <Wire.h>
+#include "MAX30105.h" // LIBRERIA SENSOR DE OXIGENO
+// #include <Adafruit_GFX.h>  // OLED libreria
+#include <Adafruit_SSD1306.h>
+#include "heartRate.h"
 
-// Claves para conectarse al internet
-char* ssid = "Adro312";   //Nombre de la red Ex. "upsrj_ALUMNOS_campus"
-char* password = "4dRo312_E";
+MAX30105 Sensor;
 
-// Inicializas el objeto 'http' con los datos de la conexion a internet
-EasyHTTP http(ssid, password);
+const byte RATE_SIZE = 4;   // incremento para obtener mas promedio
+byte rates[RATE_SIZE];      // matriz de frecuencia cardiacas
+byte rateSpot = 0;
+long ultimolatido = 0;      // hora a la que ocurrio el ultimo latido
+float latidosporminutos;
+int mejorpromedio;
 
-// Se guarda la URL de la API
-String API_URL = "http://192.168.1.112/Oximetro-API/";
+double oxi;
 
-void setup() {
-  Serial.begin(115200);
+#define SCREEN_WIDTH 128 // Ancho de la pantalla OLED en pixeles
+#define SCREEN_HEIGHT 64 // Altura de la pantalla OLED en pixeles   
+#define OLED_RESET -1    // Restablecer el pin # (o si -1 se comparte el pin de restablecimiento de arduino)
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // Declaracion del nombre de la visualizacion
 
-  /**
-   * 
-   * 
-   * Colar codigo del nuevo sensor
-   * 
-   * 
-   * 
-  */
+#define USEFIFO
+void setup() 
+{
+    // Iniciamos la pantalla OLED
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.display();
 
+    // Puerto I2C predeterminado, velocidad de 400 kHz
+    if ( !Sensor.begin(Wire, I2C_SPEED_FAST) ) { 
+        Serial.println("Sensor no fue encontrado, Por favor checa que todo este bien conectado.");
+        while (1);
+    }
 
-  // Realizas la conexion a la api
-  if ( http.connectWiFi() ) {
-    Serial.println("Connected!");
-    http.post("API/saveData", "1");
-  } else {
-    Serial.println("Internet don't found");
-  }
+    // byte leadBrightness = 0x7F; // Opciones: 0 = Apagado a 255 = 50mA
+    byte leadBrightness = 70; // Opciones: 0 = Apagado a 255 = 50mA
+    byte sampleAverage = 4; // Opciones: 1, 2, 4, 8, 16, 32
+    byte ledMode = 2; // OPciones: 1 = Solo rojo, 2 = Rojo + IR, 3 = Rojo + IR + Verde
+    int sampleRate = 200; // Opciones: 50, 100, 200, 400, 800, 1000, 1600, 3200
+    int pulseWidth = 411; // Opciones 69, 118, 215, 411
+    int adcRange = 4096; // Opciones: 2048, 4096, 8192, 16384
 
-  http.setBaseURL(API_URL);
+    // Configurar los parametro deseados
+    Sensor.setup(leadBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); // Configurar el sensor con estos ajustes
 }
 
-void loop() {
+double avered = 0;
+double aveir = 0;
+double sumirrms = 0;
+double sumredrms = 0;
+int i = 0;
+int Num = 100; // Calcular SpO2 por este intervalo de muestreo
 
-  // Verificamos que el usuario haya cambiado el estatus para activar el oximetro
-  String status_oximetro = http.get("API/getStatus");
+double ESpO2 = 93.0; // Valor inicial de la SpO2 estimada
+double FSpO2 = 0.7; // Factor de filtro para la SpO2 estimada
+double frate = 0.95; // Filtro de paso bajo para valor de LED IR / rojo para eliminar el componente de CA
+#define TIMETOBOOT 2000 // Espere este tiempo (mseg) para generar SpO2
+#define SCALE 88.0 // Ajustar para mostrar el latido del corazon y la SpO2 en la misma escala
+#define SAMPLING 5 // Si desea ver los latidos del corazon con mayor precision, configure MUESTREO en 1
+#define FINGER_ON 35000 // Si la señal roja es mas baja que esto, indica que su dedo no esta en el sensor
+#define MINIMUM_SPO2 0.0
 
-  // Si el estado es 1, se ejecutara lo siguiente
-  if (status_oximetro == "1") {
+void loop() 
+{
+    uint32_t ir, red, green;
+    double fred, fir;
+    double SpO2 = 0; // SpO2 antes de filtrado de paso bajo
 
-    // Declaramos las variables que vamos a enviar del sensor
-    float hr = 0;
-    float oxi = 0;
-    float temp = 0;
+    #ifdef USEFIFO
+        Sensor.check(); // Verifica el sensor, lea hasta 3 muestras
+        while (Sensor.available()); // Tenemos nuevos datos
+    #ifdef MAX30105
+        red = Sensor.getFIFORed(); // Sparkfun's MAX30105
+        ir = Sensor.getFIFOIR(); // Sparkfun's MAX30105
+    #else
+        red = Sensor.getFIFOIR(); // Why getFOFORed output IR data by MAX30102 on MH-ET LIVE breakout board
+        ir = Sensor.getFIFORed(); // Why getFOFOIR output Red data by MAX30102 on MH-ET LIVE breakout board
+    #endif
+        i++;
+        fred = (double)red;
+        fir = (double)ir;
+        avered = avered * frate + (double)red * (1.0 - frate); // Nivel promedio de rojo por filtro de paso bajo
+        aveir = aveir * frate + (double)ir * (1.0 - frate); // Nivel de IR promedio por filtro de paso bajo
+        sumredrms += (fred - avered) * (fred - avered); // Suma cuadrada del componente alternativo del nivel rojo
+        sumirrms += (fir - aveir) * (fir - aveir); // Suma cuadrada del componente alternativo del nivel de IR
 
+        // Ralentizar la velocidad de trazado del grafico para el trazador serial arduino por adelgazamiento
+        if ( (i % SAMPLING) == 0 ) { 
+            if ( millis() > TIMETOBOOT ) {
+                if (ir < FINGER_ON) ESpO2 = MINIMUM_SPO2;
+                if (ESpO2 <= -1) {
+                    ESpO2 = 0;
+                }
+                if (ESpO2 > 100) {
+                    ESpO2 = 100;
+                }
 
-    /**
-     * 
-     * 
-     * Colar codigo del nuevo sensor
-     * 
-     * 
-     * 
-    */
+                oxi = ESpO2;
+                Serial.print("OXIGENO % = ");
+                Serial.println(ESpO2);
+            }
+        }
+        if ( (i % Num) == 0 ) {
+            double R = (sqrt(sumredrms) / avered / (sqrt(sumirrms)) / aveir);
+            SpO2 = -23.3 * (R - 0.4) + 100; // http://wwl.microchip.com/downloads/jp/AppNotes/00001525B_JP.pdf
+            ESpO2 = FSpO2 * ESpO2 + (1.0 - FSpO2) * SpO2; // Filtro de paso bajo
+            Serial.print("OXIGENO % = ");
+            Serial.println(ESpO2);
+            sumredrms = 0.0;
+            sumirrms = 0.0;
+            i = 0;
+        }
 
+        Sensor.nextSample(); // Hemos terminado con esta muestra, asi que pade a la siguiente 
 
-    // Se nicializa la variable 'doc1' con 128kb de espacio (El default es 32 y te dará error si lo cambias)
-    // De nuevo ten cuidado con el espacio en memoria xd
-    DynamicJsonDocument doc1(128);
+        // ============================================================
+        long irHR = Sensor.getIR();
 
-    // Inicializas una cadena llamada payload que tendra los datos que vas a mandar por POST
-    String payload1 = "";
+        if (irHR > 7000) { // si se detecta un dedo
+            display.clearDisplay(); // limpiar la pantalla
+            display.setTextSize(1); // cerca de el muestra el promedio de BPM, puede mostrar el BPM si lo desea
+            display.setTextColor(WHITE);
+            display.setCursor(45, 2);
+            display.setRotation(3);
+            display.println("HR");
+            display.setRotation(0);
+            display.setTextSize(3);
+            display.setCursor(20, 5);   
+            display.println(mejorpromedio);
+            display.setCursor(107, 5);
+            display.setTextSize(1);
+            display.print("bpm");    
 
-    doc1["oxigenacion"] = oxi;
-    doc1["ritmo_cardiaco"] = hr;
-    doc1["temperatura"] = temp;
-    
-    serializeJson(doc1, payload1); // Hace la conversión
-    
-    String response1 = http.post("API/saveData", payload1); // Url exacta y datos a mandar
-    Serial.println(response1);
-    Serial.println(payload1);
-  }
+            display.setTextSize(1); // Cerca de el muestra el BPM promedio, puede mostrar el BPM si lo desea
+            display.setTextColor(WHITE);
+            display.setRotation(3);
+            display.setCursor(5, 2);
+            display.println("SpO2");
+            display.setTextSize(3);
+            display.setRotation(0);
+            display.setCursor(20, 5); 
+            display.println(oxi, 1);
+            display.setCursor(120, 35);
+            display.setTextSize(1);
+            display.print("%");
+            display.display();
+        }
 
-  delay(3000);
+        if ( irHR < 7000 ) { // Si no detecta un dedo
+            mejorpromedio = 0; //reiniciar BPM promedio 
+            display.clearDisplay();
+            display.setTextSize(2);
+            display.setTextColor(WHITE);
+            display.setCursor(15,7);
+            display.println("OxiCare");
+            display.setCursor(30,35);
+            display.setTextSize(3);
+            display.println("by Deinev");
+
+            display.display();
+        }
+    #endif
 }
